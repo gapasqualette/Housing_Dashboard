aa06109ec6b97173d33ba618ff45205ff0c3469a
Guilherme Pasqualette <guilhermepasqualette@gmail.com>
2026-04-03 21:11:49 -0300

    Making first corrections

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 us_states_and_territories_military = {
     # 50 U.S. States
     "AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", "CA": "California",
@@ -34,7 +35,15 @@ if 'states_selected' not in st.session_state:
 df = pd.read_csv("housing.csv")
 df.dropna(inplace=True)
 
-df = df.rename({df.columns[0]: "AvgArea (sqft)", df.columns[1]: "AvgAge (years)", df.columns[2]: "AvgRooms", df.columns[3]: "AvgBedrooms"}, axis='columns')
+df = df.rename(
+    {
+        df.columns[0]: "area_income", 
+        df.columns[1]: "house_age", 
+        df.columns[2]: "avg_rooms", 
+        df.columns[3]: "avg_bedrooms"
+        }, 
+        axis='columns'
+    )
 
 df = df.round(2)
 
@@ -42,9 +51,20 @@ df["Street"] = df["Address"].str.split(",").str[0]
 
 df["State"] = df["Address"].str.split().str[-2]
 
-df = df.drop(["Address"], axis=1)
+df['n_people'] = df['avg_bedrooms'].astype(int) + 1
 
-cols_num = ["AvgArea (sqft)", "AvgAge (years)", "AvgRooms", "AvgBedrooms", "Area Population", "Price"]
+df['income_house'] = np.round(df['area_income'] / df['n_people'], 2)
+
+# Income Range
+df['income_range'] = pd.cut(
+    df['area_income'],
+    bins=[0, 60000, 72500, 85000, np.inf],
+    labels=['Low', 'Medium', 'High', 'Rich']
+)
+
+df['prop_appr'] = np.round(df['Price'] / df['house_age'], 2)
+
+df = df.drop(["Address"], axis=1)
 
 st.header(f"**USA Housing Dashboard** - **Housing.csv On Kaggle**", anchor=False, divider=True)
 
@@ -69,9 +89,9 @@ column = st.columns(4)
 with column[0]:
     tile = column[0].container(height='stretch', border=True)
     tile.subheader("Area Stats 🌎", divider=True)
-    tile.write(f'**Average Area**: **{np.mean(df['AvgArea (sqft)']):,.2f}** sqft')
-    tile.write(f'**Smallest Area**: **{np.min(df["AvgArea (sqft)"]):,.2f}** sqft')
-    tile.write(f'**Biggest Area**: **{np.max(df["AvgArea (sqft)"]):,.2f}** sqft')
+    tile.write(f'**Average Income**: **US${np.mean(df['area_income']):,.2f}**')
+    tile.write(f'**Smallest Income**: **US${np.min(df["area_income"]):,.2f}**')
+    tile.write(f'**Biggest Income**: **US${np.max(df["area_income"]):,.2f}**')
 
 with column[1]:
     tile2 = column[1].container(height='stretch', border=True)
@@ -95,43 +115,58 @@ with column[3]:
 
 st.divider()
 
-btn_columns = st.columns([45,5,50])
-tile_btns = btn_columns[0].container(height=150)
-tile2_btns = btn_columns[0].container()
 abbr_state = []
 code_states = []
 
 for state in us_states_and_territories_military:
     abbr_state.append(f'{state} - {us_states_and_territories_military.get(state,)}')
 
-with tile_btns:
-    st.session_state.states_selected = tile_btns.multiselect('Choose the states', abbr_state)
+st.session_state.states_selected = st.multiselect('Choose the states', abbr_state)
 
-with tile2_btns:
-    if len(st.session_state.states_selected) > 1:
-        states = [state.split(" - ")[1] for state in st.session_state.states_selected]
-        str_states = ' | '.join(sorted(states))
-        st.subheader(f'Chosen states: {str_states}', divider=True)
-        code_states = [state.split(" - ")[0] for state in st.session_state.states_selected]
+if len(st.session_state.states_selected) > 1:
+    states = [state.split(" - ")[1] for state in st.session_state.states_selected]
+    str_states = ' | '.join(sorted(states))
+    st.subheader(f'Chosen states: {str_states}', divider=True)
+    code_states = [state.split(" - ")[0] for state in st.session_state.states_selected]
+    cols_expander = st.columns(2)
 
-        #################### Area Section ##############################
+    #################### Income Section ##############################
+    with cols_expander[0]:
         with st.expander('Area 🌎', expanded=True):
-            areas = [df[df["State"] == estado]["AvgArea (sqft)"].mean() for estado in code_states]
-            st.write(f'**Average States Area**: **{np.mean(areas):,.2f}** sqft')
+            income = [df[df["State"] == estado]["area_income"].mean() for estado in code_states]
+            st.write(f'**Average States Income**: **US${np.mean(income):,.2f}**')
 
-            area_fewer = min(code_states, key= lambda estado: df[df["State"] == estado]["AvgArea (sqft)"].values[0])
-            area_fewer_data = df[df["State"] == area_fewer].nsmallest(1, "AvgArea (sqft)")
-            area_fewer_street = area_fewer_data["Street"].values[0]
-            area_fewer_area = area_fewer_data["AvgArea (sqft)"].values[0]
-            st.write(f'**Smallest Area** in **{area_fewer_street}** - **{area_fewer}** with **{area_fewer_area:,.2f}** sqft')
+            income_fewer = min(code_states, key= lambda estado: df[df["State"] == estado]["area_income"].values[0])
+            income_fewer_data = df[df["State"] == income_fewer].nsmallest(1, "area_income")
+            income_fewer_street = income_fewer_data["Street"].values[0]
+            income_fewer_area = income_fewer_data["area_income"].values[0]
+            st.write(f'**Smallest Income** in **{income_fewer_street}** - **{income_fewer}** with **US${income_fewer_area:,.2f}**')
 
-            área_max = max(code_states, key= lambda estado: df[df["State"] == estado]["AvgArea (sqft)"].values[0])
-            area_max_data = df[df["State"] == área_max].nlargest(1, "AvgArea (sqft)")
-            área_max_street = area_max_data["Street"].values[0]
-            área_max_area = area_max_data["AvgArea (sqft)"].values[0]
-            st.write(f'**Biggest Area** in **{área_max_street}** - **{área_max}** with **{área_max_area:,.2f}** sqft')
+            income_max = max(code_states, key= lambda estado: df[df["State"] == estado]["area_income"].values[0])
+            income_max_data = df[df["State"] == income_max].nlargest(1, "area_income")
+            income_max_street = income_max_data["Street"].values[0]
+            income_max_area = income_max_data["area_income"].values[0]
+            st.write(f'**Biggest Income** in **{income_max_street}** - **{income_max}** with **US${income_max_area:,.2f}**')
+
+        with st.expander('Price 💵', expanded=True):
+            prices = [df[df["State"] == estado]['Price'].mean() for estado in code_states]
+            st.write(f'**Average States Price**: **US${np.mean(prices): ,.2f}**')
+
+            price_fewer = min(code_states, key= lambda estado: df[df["State"] == estado]["Price"].values[0])
+            price_fewer_data = df[df["State"] == price_fewer].nsmallest(1, "Price")
+            price_fewer_street = price_fewer_data["Street"].values[0]
+            price_fewer_price = price_fewer_data["Price"].values[0]
+            st.write(f'**Smallest Price** in **{price_fewer_street}** - **{price_fewer}** costing **US${price_fewer_price: ,.2f}**')
+
+            price_max = max(code_states, key= lambda estado: df[df["State"] == estado]["Price"].values[0])
+            price_max_data = df[df["State"] == price_max].nlargest(1, "Price")
+            price_max_street = price_max_data['Street'].values[0]
+            price_max_price = price_max_data['Price'].values[0]
+            st.write(f'**Biggest Population** in **{price_max_street}** - **{price_max}** costing **US${price_max_price: ,.2f}**')
+
 
         ####################### Population Section #############################
+    with cols_expander[1]:   
         with st.expander('Population 👨‍👩‍👧‍👦', expanded=True):
             populations = [df[df["State"] == estado]['Area Population'].mean() for estado in code_states]
             st.write(f'**Average Areas Population**: **{int(np.mean(populations))}**')
@@ -149,68 +184,105 @@ with tile2_btns:
             st.write(f'**Biggest Population** in **{pop_max_street}** - **{pop_max}** with **{pop_max_pop}** People')
 
         ######################## Price Section ##################################
-        with st.expander('Price 💵', expanded=True):
-            prices = [df[df["State"] == estado]['Price'].mean() for estado in code_states]
-            st.write(f'**Average States Price**: **US${np.mean(prices): ,.2f}**')
-
-            price_fewer = min(code_states, key= lambda estado: df[df["State"] == estado]["Price"].values[0])
-            price_fewer_data = df[df["State"] == price_fewer].nsmallest(1, "Price")
-            price_fewer_street = price_fewer_data["Street"].values[0]
-            price_fewer_price = price_fewer_data["Price"].values[0]
-            st.write(f'**Smallest Price** in **{price_fewer_street}** - **{price_fewer}** costing **US${price_fewer_price: ,.2f}**')
-
-            price_max = max(code_states, key= lambda estado: df[df["State"] == estado]["Price"].values[0])
-            price_max_data = df[df["State"] == price_max].nlargest(1, "Price")
-            price_max_street = price_max_data['Street'].values[0]
-            price_max_price = price_max_data['Price'].values[0]
-            st.write(f'**Biggest Population** in **{price_max_street}** - **{price_max}** costing **US${price_max_price: ,.2f}**')
-
+        
         with st.expander('Rooms & Bedrooms 💻', expanded=True):
-            rooms = [df[df["State"] == estado]['AvgRooms'].mean() for estado in code_states]
-            bedrooms = [df[df["State"] == estado]['AvgBedrooms'].mean() for estado in code_states]
+            rooms = [df[df["State"] == estado]['avg_rooms'].mean() for estado in code_states]
+            bedrooms = [df[df["State"] == estado]['avg_bedrooms'].mean() for estado in code_states]
             st.write(f'**Average States Number of Rooms**: **{np.mean(rooms):,.2f}**')
-            room_min = min(code_states, key= lambda estado: df[df["State"] == estado]['AvgRooms'].values[0])
-            room_min_data = df[df["State"] == room_min].nsmallest(1,'AvgRooms')
+            room_min = min(code_states, key= lambda estado: df[df["State"] == estado]['avg_rooms'].values[0])
+            room_min_data = df[df["State"] == room_min].nsmallest(1,'avg_rooms')
             room_min_street = room_min_data['Street'].values[0]
-            room_min_room = room_min_data['AvgRooms'].values[0]
+            room_min_room = room_min_data['avg_rooms'].values[0]
             st.write(f'**Smallest Number of Rooms** are in **{room_min_street}** - **{room_min}** with **{room_min_room}** rooms')
 
-            room_max = max(code_states, key= lambda estado: df[df["State"] == estado]['AvgRooms'].values[0])
-            room_max_data = df[df["State"] == room_max].nlargest(1,'AvgRooms')
+            room_max = max(code_states, key= lambda estado: df[df["State"] == estado]['avg_rooms'].values[0])
+            room_max_data = df[df["State"] == room_max].nlargest(1,'avg_rooms')
             room_max_street = room_max_data['Street'].values[0]
-            room_max_room = room_max_data['AvgRooms'].values[0]
+            room_max_room = room_max_data['avg_rooms'].values[0]
             st.write(f'**Highest Number of Rooms** are in **{room_max_street}** - **{room_max}** with **{room_max_room}** rooms')
 
             st.write(f'**Average States Number of Bedrooms**: **{np.mean(bedrooms):,.2f}**')
-            bedroom_min = min(code_states, key= lambda estado: df[df["State"] == estado]['AvgBedrooms'].values[0])
-            bedroom_min_data = df[df["State"] == bedroom_min].nsmallest(1,'AvgBedrooms')
+            bedroom_min = min(code_states, key= lambda estado: df[df["State"] == estado]['avg_bedrooms'].values[0])
+            bedroom_min_data = df[df["State"] == bedroom_min].nsmallest(1,'avg_bedrooms')
             bedroom_min_street = bedroom_min_data['Street'].values[0]
-            bedroom_min_bedrooms = bedroom_min_data['AvgBedrooms'].values[0]
+            bedroom_min_bedrooms = bedroom_min_data['avg_bedrooms'].values[0]
             st.write(f'**Smallest Number of Bedrooms** are in **{bedroom_min_street}** - **{bedroom_min}** with **{bedroom_min_bedrooms}** Bedrooms')
 
-            bedroom_max = min(code_states, key= lambda estado: df[df["State"] == estado]['AvgBedrooms'].values[0])
-            bedroom_max_data = df[df["State"] == bedroom_max].nlargest(1,'AvgBedrooms')
+            bedroom_max = min(code_states, key= lambda estado: df[df["State"] == estado]['avg_bedrooms'].values[0])
+            bedroom_max_data = df[df["State"] == bedroom_max].nlargest(1,'avg_bedrooms')
             bedroom_max_street = bedroom_max_data['Street'].values[0]
-            bedroom_max_bedrooms = bedroom_max_data['AvgBedrooms'].values[0]
+            bedroom_max_bedrooms = bedroom_max_data['avg_bedrooms'].values[0]
             st.write(f'**Highest Number of Bedrooms** are in **{bedroom_max_street}** - **{bedroom_max}** with **{bedroom_max_bedrooms}** Bedrooms')
 
 
-        with btn_columns[1]:
-            st.markdown('<div class = "vl"></div>', unsafe_allow_html=True)
 
-        with btn_columns[2]:
-            df_filter = df[df["State"].isin(code_states)]
-            df_filter = df_filter.groupby("State")[cols_num].mean()
+df_filter = df[df["State"].isin(code_states)]
+df_filter_avg = df_filter.groupby('State')[['area_income', 'house_age', 'avg_rooms', 'avg_bedrooms', 'Area Population', 'Price', 'prop_appr']].mean().round(2)
 
-            st.subheader('Areas & Populations Comparisons 📈')
-            chart_area = df_filter[['AvgArea (sqft)', 'Area Population']]
-            st.line_chart(chart_area, x_label='Data per State', y_label='Average Values')
+if len(st.session_state.states_selected) > 1:
+    st.subheader('Plots', divider=True)    
+    btn_columns = st.columns([49,2,49])
+    with btn_columns[0]:
+        fig, ax = plt.subplots(figsize=(10,6))
+        sns.barplot(df_filter_avg, x='State', y='Price', linewidth = 1.5, edgecolor = 'black', )
+        ax.set_title('Plot 1 - Average Price per State & Number of Rooms')
+        ax.set_xlabel('States')
+        ax.set_ylabel('Average Price')
+        ax.set_ylim(bottom=df_filter_avg['Price'].min() - 50000)
+        ax2 = ax.twinx()
+        sns.lineplot(df_filter_avg, x='State', y='avg_rooms', marker='o', linewidth = 3, color = 'red')
 
-            st.subheader('Rooms & Bedrooms Comparisons 📉')
-            chart_rooms = df_filter[['AvgRooms', 'AvgBedrooms']]
-            st.line_chart(chart_rooms, x_label='Data per State', y_label='Average Values')
+        ax2.set_ylabel('Average Number of Rooms', color = 'red')
 
-            st.subheader('Prices Comparisons 📊')
-            chart_price = df_filter[['Price']]
-            st.bar_chart(chart_price, x_label='Data per State', y_label='Average Values', height=200)
-            
+        with st.container(height='stretch', width='stretch', border=True):
+            st.pyplot(fig)
+
+        df_profile = df_filter.groupby(['State', 'income_range']).size().reset_index(name='count_profiles')
+
+        fig, ax = plt.subplots(figsize=(7,3))
+        sns.barplot(df_profile, x='State', y='count_profiles', linewidth = 1.25, edgecolor = 'black', hue='income_range', palette='pastel')
+        ax.set_xlabel('State')
+        ax.set_ylabel('Profile Count')
+        ax.set_title('Plot 3 - Profile Count per State')
+
+        with st.container(height='stretch', width='stretch', border=True):
+            st.pyplot(fig)
+
+    with btn_columns[1]:
+        st.html(
+        """<div style="
+            border-left: 3px solid red;
+            height: 1000px;
+            margin: auto;
+            width: 0;
+            "></div>
+        """ 
+    )
+
+    with btn_columns[2]:
+        fig, ax = plt.subplots(figsize=(7,3))
+        sns.barplot(df_filter_avg, x='State', y='area_income', linewidth = 1.5, edgecolor = 'black', )
+        ax.set_title('Plot 2 - Average Income per State & Number of Rooms')
+        ax.set_xlabel('States')
+        ax.set_ylabel('Average Income')
+        ax.set_ylim(bottom=df_filter_avg['area_income'].min() - 5000)
+        ax2 = ax.twinx()
+        sns.lineplot(df_filter_avg, x='State', y='avg_rooms', marker='o', linewidth = 3, color = 'red')
+
+        ax2.set_ylabel('Average Number of Rooms', color = 'red')
+
+        with st.container(height='stretch', width='stretch', border=True):
+            st.pyplot(fig)
+        #####
+
+        #####
+
+        fig, ax = plt.subplots(figsize=(7,3))
+        sns.barplot(df_filter_avg, x='State', y='prop_appr', linewidth = 2, edgecolor = 'black', palette='pastel')
+        ax.set_xlabel('State')
+        ax.set_ylabel('Property Appreciation')
+        ax.set_title('Plot 4 - Property Appreciation (PRICE / AGE) per State')
+        ax.set_ylim(df_filter_avg['prop_appr'].min() - 10000)
+
+        with st.container(height='stretch', width='stretch', border=True):
+            st.pyplot(fig)
